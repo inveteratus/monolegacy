a5469de759bc5161a42e160370a730271f6e7d6d
Inveteratus <alan.mcfarlane@gmail.com>
2024-09-03 18:14:56 +0100

    Added initial config

---
+++ public/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$_CONFIG = array(
+    'hostname' => '127.0.0.1',
+    'username' => 'monolegacy',
+    'password' => '****************',
+    'database' => 'monolegacy',
+    'persistent' => false,
+    'driver' => 'mysqli',
+    'code' => random_bytes(32),
+);
