49627258c509354a8685e1ab77541be88930fffa
Inveteratus <alan.mcfarlane@gmail.com>
2024-09-03 17:47:33 +0100

    Correct variable names

--- public/staff_crimes.php
+++ public/staff_crimes.php
@@ -885,12 +885,12 @@ function reorder_crimegroups()
                          WHERE `cgORDER` IN(' . $ro . ')');
         if ($db->fetch_single($c_g) < $ro_cnt)
         {
-            $db->free_result($c_q);
+            $db->free_result($c_g);
             echo 'Group order doesn\'t exist.<br />
             &gt; <a href="staff_crimes.php?action=reorder">Go Back</a>';
             die($h->endpage());
         }
-        $db->free_result($c_q);
+        $db->free_result($c_g);
         foreach ($_POST as $k => $v)
         {
             $cg = str_replace("order", "", $k);
