902e3e831f197d983d77f2a95ee585a1fafd14cf
Inveteratus <alan.mcfarlane@gmail.com>
2024-09-06 19:25:44 +0100

    Typo

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 * Forked from [davemacaulay/mccodesv2](https://github.com/davemacaulay/mccodesv2).
 * Reset to v2.0.5b to remove both Dave Macaulay's and Magictallguy's alterations. (Sorry guys!)
-* Stared migration to using the [Slim/Framework](https://www.slimframework.com/).
+* Started to migrate to the [Slim/Framework](https://www.slimframework.com/).
 
 ## Requirements
 
---
+++ src/Middleware/RegenerateMiddleware.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Middleware;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Psr\Http\Message\ResponseInterface as Response;
+
+class RegenerateMiddleware
+{
+    public function __invoke(Request $request, RequestHandler $handler): Response
+    {
+        session_start(['name' => 'MCCSID']);
+
+        return $handler->handle($request);
+    }
+}
