08a5eba930b4ddfc817975bf4fa49298fcb7b981
Inveteratus <alan.mcfarlane@gmail.com>
2024-09-13 09:00:32 +0100

    Moved routes

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
-use App\Http\Requests\RegisterRequest;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,10 @@
 <?php
 
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\LogoutController;
-use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
     Route::view('/', 'index')->name('index');
-    Route::view('login', 'login')->name('login');
-    Route::post('login', LoginController::class);
-    Route::view('register', 'register')->name('register');
-    Route::post('register', RegisterController::class);
 });
-
-Route::post('logout', LogoutController::class)->name('logout');
 
 Route::middleware('auth')->group(function () {
     Route::view('home', 'home')->name('home');
