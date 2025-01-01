170849785da39b889e195ceca23d5874d956860c
Inveteratus <alan.mcfarlane@gmail.com>
2025-01-01 22:49:53 +0000

    Styling

--- public/header.php
+++ public/header.php
@@ -4,7 +4,14 @@ class headers
 {
     function startheaders()
     {
-        global $ir, $set;
+        global $db, $ir, $set, $userid;
+
+        $format = fn ($value) => $value ? (' (' . number_format($value) . ')') : '';
+        $hospital = $format($db->execute('SELECT COUNT(*) FROM users WHERE hospital > 0')->fetchColumn());
+        $jail = $format($db->execute('SELECT COUNT(*) FROM users WHERE jail > 0')->fetchColumn());
+        $events = $format($db->execute('SELECT COUNT(*) FROM events WHERE evUSER = :uid and evREAD = 0', ['uid' => $userid])->fetchColumn());
+        $mail = $format($db->execute('SELECT COUNT(*) FROM mail WHERE mail_to = :uid and mail_read = 0', ['uid' => $userid])->fetchColumn());
+
         echo <<<HTML
             <!DOCTYPE html>
             <html lang="en-GB">
@@ -19,7 +26,26 @@ class headers
             </head>
             <body class="bg-slate-200">
                 <div class="max-w-5xl mx-auto shadow-xl">
-                    <img src="title.jpg" class="w-full" alt="Monolegacy" />
+                    <div class="bg-teal-500 h-16 flex justify-between items-center px-3">
+                        <div class="flex items-center">
+                            <a href="/" class="text-teal-900 hover:text-teal-200 p-3">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                                </svg>
+                            </a>
+                            <a href="/explore.php" class="text-teal-900 hover:text-teal-200 p-3">Explore</a>
+                            <a href="/inventory.php" class="text-teal-900 hover:text-teal-200 p-3">Inventory</a>
+                            <span class="border-r border-teal-600 text-sm pl-1 mr-1">&nbsp;</span>
+                            <a href="/hospital.php" class="text-teal-900 hover:text-teal-200 p-3">Hospital{$hospital}</a>
+                            <a href="/jail.php" class="text-teal-900 hover:text-teal-200 p-3">Jail{$jail}</a>
+                            <span class="border-r border-teal-600 text-sm pl-1 mr-1">&nbsp;</span>
+                            <a href="/events.php" class="text-teal-900 hover:text-teal-200 p-3">Events{$events}</a>
+                            <a href="/forums.php" class="text-teal-900 hover:text-teal-200 p-3">Forum</a>
+                            <a href="/mailbox.php" class="text-teal-900 hover:text-teal-200 p-3">Mail{$mail}</a>
+                        </div>
+                        <div class="flex items-center">
+                        </div>
+                    </div>
                     <div class="flex">
         HTML;
     }
@@ -45,11 +71,19 @@ class headers
                      WHERE `userid` = $userid");
             $_SESSION['attacking'] = 0;
         }
-        $enperc = min((int) ($ir['energy'] / $ir['maxenergy'] * 100), 100);
-        $wiperc = min((int) ($ir['will'] / $ir['maxwill'] * 100), 100);
-        $experc = min((int) ($ir['exp'] / $ir['exp_needed'] * 100), 100);
-        $brperc = min((int) ($ir['brave'] / $ir['maxbrave'] * 100), 100);
-        $hpperc = min((int) ($ir['hp'] / $ir['maxhp'] * 100), 100);
+
+        $ep = ($ir['energy'] * 100) / $ir['maxenergy'];
+        $ef = number_format($ep, 2) . ' %';
+        $np = ($ir['brave'] * 100) / $ir['maxbrave'];
+        $nf = number_format($ir['brave']) . ' / ' . number_format($ir['maxbrave']);
+        $hp = ($ir['hp'] * 100) / $ir['maxhp'];
+        $hf = number_format($hp, 2) . ' %';
+        $dp = ($ir['will'] * 100) / $ir['maxwill'];
+        $df = number_format($ir['will']) . ' / ' . number_format($ir['maxwill']);
+        $xp = min(($ir['exp'] * 100) / $ir['exp_needed'], 100);
+        $xf = $ir['exp'] >= $ir['exp_needed'] ? '<a href="/upgrade.php" class="text-blue-700 hover:underline">Upgrade</a>' : ('Level ' . number_format($ir['level']));
+        $cr = number_format($ir['money']) . ' Cr';
+
         $d = "";
         $u = $ir['username'];
         if ($ir['donatordays'])
@@ -64,23 +98,32 @@ class headers
         $gn = "";
 
         echo <<<HTML
-            <aside class="bg-slate-100 border-r border-slate-300 flex-none p-4 text-center w-72">
-                <b>Name:</b> $gn{$u} [{$ir['userid']}] $d<br />
-                <b>Money:</b> {$fm}<br />
-                <b>Level:</b> {$ir['level']}<br />
-                <b>Crystals:</b> {$ir['crystals']}<br />
-                [<a href='logout.php'>Emergency Logout</a>]
-                <hr />
-                <b>Energy:</b> {$enperc}%<br />
-                <div class="bg-slate-200 h-2.5 mx-3"><span class="bg-red-500 h-2.5 block" style="width:{$enperc}%"></span></div>
-                <b>Will:</b> {$wiperc}%<br />
-                <div class="bg-slate-200 h-2.5 mx-3"><span class="bg-blue-500 h-2.5 block" style="width:{$wiperc}%"></span></div>
-                <b>Brave:</b> {$ir['brave']}/{$ir['maxbrave']}<br />
-                <div class="bg-slate-200 h-2.5 mx-3"><span class="bg-yellow-500 h-2.5 block" style="width:{$brperc}%"></span></div>
-                <b>EXP:</b> {$experc}%<br />
-                <div class="bg-slate-200 h-2.5 mx-3"><span class="bg-indigo-500 h-2.5 block" style="width:{$experc}%"></span></div>
-                <b>Health:</b> {$hpperc}%<br />
-                <div class="bg-slate-200 h-2.5 mx-3"><span class="bg-green-500 h-2.5 block" style="width:{$hpperc}%"></span></div>
+            <aside class="bg-slate-100 border-r border-slate-300 flex-none p-4 text-center text-slate-700 w-72">
+                <div class="grid grid-cols-2">
+                    <span class="text-left">Name</span>
+                    <span class="text-right">$gn{$u} [{$ir['userid']}] $d</span>
+                    <span class="text-left">Money</span>
+                    <span class="text-right">{$cr}</span>
+                    <span class="text-left">Diamonds</span>
+                    <span class="text-right">{$ir['crystals']}</span>
+                </div>
+                
+                <hr class="my-2" />
+                
+                <div class="bg-slate-200 h-2.5 mt-2"><span class="bg-red-500 h-2.5 block" style="width:{$ep}%"></span></div>
+                <p class="flex items-center justify-between"><span>Energy</span><span>{$ef}</span></p>
+
+                <div class="bg-slate-200 h-2.5 mt-2"><span class="bg-yellow-500 h-2.5 block" style="width:{$np}%"></span></div>
+                <p class="flex items-center justify-between"><span>Nerve</span><span>{$nf}</span></p>
+
+                <div class="bg-slate-200 h-2.5 mt-2"><span class="bg-green-500 h-2.5 block" style="width:{$hp}%"></span></div>
+                <p class="flex items-center justify-between"><span>Health</span><span>{$hf}</span></p>
+
+                <div class="bg-slate-200 h-2.5 mt-2"><span class="bg-blue-500 h-2.5 block" style="width:{$dp}%"></span></div>
+                <p class="flex items-center justify-between"><span>Drive</span><span>{$df}</span></p>
+
+                <div class="bg-slate-200 h-2.5 mt-2"><span class="bg-indigo-500 h-2.5 block" style="width:{$xp}%"></span></div>
+                <p class="flex items-center justify-between"><span>Experience</span><span>{$xf}</span></p>
         HTML;
     }
 
--- public/mainmenu.php
+++ public/mainmenu.php
@@ -2,25 +2,13 @@
 
 global $db, $ir, $userid;
 
-$hc = number_format($db->execute('SELECT COUNT(*) FROM users WHERE hospital > 0')->fetchColumn());
-$jc = number_format($db->execute('SELECT COUNT(*) FROM users WHERE jail > 0')->fetchColumn());
-$ec = number_format($db->execute('SELECT COUNT(*) FROM events WHERE evUSER = :uid and evREAD = 0', ['uid' => $userid])->fetchColumn());
-$mc = number_format($db->execute('SELECT COUNT(*) FROM mail WHERE mail_to = :uid and mail_read = 0', ['uid' => $userid])->fetchColumn());
 $ac = number_format($ir['new_announcements']);
 
 echo <<<HTML
-    <a href='/index.php'>Home</a><br />
-    <a href='/inventory.php'>Inventory</a><br />
-    <a href="/events.php">Events ({$ec})</a><br />
-    <a href="/mailbox.php">Mailbox ({$mc})</a><br />
-	<a href="/explore.php">Explore</a><br />
 	<a href="/gym.php">Gym</a><br />
 	<a href="/criminal.php">Crimes</a><br />
 	<a href="/job.php">Your Job</a><br />
 	<a href="/education.php">Local School</a><br />
-	<a href="/hospital.php">Hospital ({$hc})</a><br />
-	<a href="/jail.php">Jail ({$jc})</a><br />
-    <a href="/forums.php">Forums</a><br />
     <a href="/announcements.php">Announcements ({$ac})</a><br />
     <a href="/newspaper.php">Newspaper</a><br />
     <a href="/search.php">Search</a><br />
