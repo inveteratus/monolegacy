01eb5152602c6134fb6a4a1ef805406a2e6090de
Inveteratus <alan.mcfarlane@gmail.com>
2025-01-01 12:40:27 +0000

    Added missing fields

--- public/register.php
+++ public/register.php
@@ -56,9 +56,6 @@ if (file_exists('ipbans/' . $IP))
 }
 $username =
         (isset($_POST['username'])
-                && preg_match(
-                        "/^[a-z0-9_]+([\\s]{1}[a-z0-9_]|[a-z0-9_])+$/i",
-                        $_POST['username'])
                 && ((strlen($_POST['username']) < 32)
                         && (strlen($_POST['username']) >= 3)))
                 ? stripslashes($_POST['username']) : '';
@@ -179,12 +176,12 @@ if (!empty($username))
                  `energy`, `maxenergy`, `will`, `maxwill`, `brave`,
                  `maxbrave`, `hp`, `maxhp`, `location`, `gender`,
                  `signedup`, `email`, `bankmoney`, `lastip`,
-                 `lastip_signup`, `pass_salt`)
+                 `lastip_signup`, `pass_salt`, `display_pic`, `staffnotes`, `voted`, `user_notepad`)
                  VALUES('{$e_username}', '{$e_username}', '{$e_encpsw}', 1,
                  $sm, 0, 0, 1, 12, 12, 100, 100, 5, 5, 100, 100, 1,
                  '{$e_gender}', " . time()
                         . ",'{$e_email}', -1, '$IP',
-                 '$IP', '{$e_salt}')");
+                 '$IP', '{$e_salt}', '', '', '', '')");
         $i = $db->insert_id();
         $db->query(
                 "INSERT INTO `userstats`
