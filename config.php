ecb07e55e3d08b1a26dba51a56fe81c14ac797f1
Inveteratus <alan.mcfarlane@gmail.com>
2025-01-01 10:47:50 +0000

    Added sample config

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$_CONFIG = [
+    'hostname' => '127.0.0.1',
+    'username' => 'monolegacy',
+    'password' => 'secret',
+    'database' => 'monolegacy',
+    'persistent' => false,
+    'driver' => 'mysqli',
+    'code' => 'secret',
+];
